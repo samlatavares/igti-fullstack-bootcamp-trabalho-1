5da140c1fd73c16e9acc629c187b9c25ebcb66ee
Samla Azevedo Tavares <samla_azevedo@outlook.com>
2020-09-22 21:53:48 -0300

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,55 @@
+var firstRange = null;
+var secondRange = null;
+var thirdRange = null;
+var firstText = null;
+var secondText = null;
+
+function start() {
+  addEvents();
+  paintDiv();
+}
+
+function addEvents() {
+  firstRange = document.querySelector('#firstRange');
+  secondRange = document.querySelector('#secondRange');
+  thirdRange = document.querySelector('#thirdRange');
+
+  firstText = document.querySelector('#firstText');
+  secondText = document.querySelector('#secondText');
+  thirdText = document.querySelector('#thirdText');
+
+  firstRange.addEventListener('input', fillFirstText);
+  secondRange.addEventListener('input', fillSecondText);
+  thirdRange.addEventListener('input', fillThirdText);
+}
+
+function fillFirstText(event) {
+  firstText.value = event.target.value;
+  paintDiv();
+}
+
+function fillSecondText(event) {
+  secondText.value = event.target.value;
+  paintDiv();
+}
+
+function fillThirdText(event) {
+  thirdText.value = event.target.value;
+  paintDiv();
+}
+
+function paintDiv() {
+  var divColor = document.querySelector('#divColor');
+  divColor.setAttribute(
+    'style',
+    'background-color:rgb(' +
+      firstText.value +
+      ',' +
+      secondText.value +
+      ',' +
+      thirdText.value +
+      ')'
+  );
+}
+
+start();
